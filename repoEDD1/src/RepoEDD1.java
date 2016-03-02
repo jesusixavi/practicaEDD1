18f24dce6592c9e570435f2cf3cd07a235a79287
jesus alvarez <jesus@jesus-HP-Pavilion-dv6-Notebook-PC>
2016-03-02 03:19:39 +0100

    hg

--- repoEDD1/src/RepoEDD1.java
+++ repoEDD1/src/RepoEDD1.java
@@ -30,13 +30,12 @@ public class RepoEDD1 {
         System.out.println("proves eclipse");
         System.out.println("proves eclipse branca");
         System.out.println("proves eclipse gasolinera");
-        System.out.println("proves eclipse gasolinera 2");
+        System.out.println("proves eclipse gasolinera 2")
         System.out.println("proves eclipse gasolinera 3");
-        System.out.println("proves netbeans gasolinera");
         System.out.println("proves eclipse gasolinera");
         System.out.println("proves eclipse gasolinera 5");
+        System.out.println("proves eclipse gasolinera6");
         
-
 
     }
     
